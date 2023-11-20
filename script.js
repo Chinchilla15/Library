e8e0f3b5db3cb95fc37b8d95f67093e7e6ffa375
Alessandro Chinchilla <ale030401@gmail.com>
2023-11-20 12:59:45 -0600

    Add: svg for book remove button and added item buttons in a div

--- script.js
+++ script.js
@@ -24,12 +24,20 @@ function displayLibrary(){
         const bookItem = document.createElement('div');
         const removeButton = document.createElement('button');
         const readButton = document.createElement('button');
+        const bookButtons = document.createElement('div');
 
-        bookItem.classList.add('bookItem')
+        bookItem.classList.add('bookItem');
+        removeButton.classList.add('removeButton');
+        bookButtons.classList.add('bookButtons');
+
+        const removeIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        removeIcon.setAttribute("fill", "currentColor");
+        removeIcon.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        removeIcon.setAttribute("viewBox", "0 0 24 24");   
+        removeIcon.innerHTML = '<path d="M13 19C13 20.1 13.3 21.12 13.81 22H6C4.89 22 4 21.11 4 20V4C4 2.9 4.89 2 6 2H7V9L9.5 7.5L12 9V2H18C19.1 2 20 2.89 20 4V13.09C19.67 13.04 19.34 13 19 13C15.69 13 13 15.69 13 19M22.54 16.88L21.12 15.47L19 17.59L16.88 15.47L15.47 16.88L17.59 19L15.47 21.12L16.88 22.54L19 20.41L21.12 22.54L22.54 21.12L20.41 19L22.54 16.88Z" />'
 
         bookItem.innerHTML = `<p>Title: ${book.title}</p><p>Author: ${book.author}</p><p>Pages:${book.pages}</p><p>Read:${book.read}</p>`
 
-        removeButton.textContent = "Remove"
         readButton.textContent = book.read === "Yes" ? "Read" : "Not Read";
 
         removeButton.addEventListener('click', function(){
@@ -45,13 +53,17 @@ function displayLibrary(){
             displayLibrary();
         })
     
+        removeButton.appendChild(removeIcon);
         bookItem.appendChild(removeButton);
         bookItem.appendChild(readButton);
+        bookItem.appendChild(bookButtons);
+        bookButtons.appendChild(removeButton);
+        bookButtons.appendChild(readButton);
 
         if(book.read === "Yes"){
             libraryContainerYes.appendChild(bookItem);
         }else{
-            libraryContainerNo.appendChild(bookItem)
+            libraryContainerNo.appendChild(bookItem);
         }
     })
 }
