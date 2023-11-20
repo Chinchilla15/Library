65200ca315af178a720bfd80b5a1e573ec92afb7
Alessandro Chinchilla <ale030401@gmail.com>
2023-11-20 15:22:59 -0600

    Add: svg for read status button and added class name

--- script.js
+++ script.js
@@ -27,8 +27,9 @@ function displayLibrary(){
         const bookButtons = document.createElement('div');
 
         bookItem.classList.add('bookItem');
-        removeButton.classList.add('removeButton');
         bookButtons.classList.add('bookButtons');
+        removeButton.classList.add('removeButton');
+        readButton.classList.add('readButton');
 
         const removeIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         removeIcon.setAttribute("fill", "currentColor");
@@ -36,9 +37,13 @@ function displayLibrary(){
         removeIcon.setAttribute("viewBox", "0 0 24 24");   
         removeIcon.innerHTML = '<path d="M13 19C13 20.1 13.3 21.12 13.81 22H6C4.89 22 4 21.11 4 20V4C4 2.9 4.89 2 6 2H7V9L9.5 7.5L12 9V2H18C19.1 2 20 2.89 20 4V13.09C19.67 13.04 19.34 13 19 13C15.69 13 13 15.69 13 19M22.54 16.88L21.12 15.47L19 17.59L16.88 15.47L15.47 16.88L17.59 19L15.47 21.12L16.88 22.54L19 20.41L21.12 22.54L22.54 21.12L20.41 19L22.54 16.88Z" />'
 
-        bookItem.innerHTML = `<p>Title: ${book.title}</p><p>Author: ${book.author}</p><p>Pages:${book.pages}</p><p>Read:${book.read}</p>`
+        const readIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        readIcon.setAttribute("fill", "currentColor");
+        readIcon.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        readIcon.setAttribute("viewBox", "0 0 24 24")
+        readIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Update Status</title><path d="M21,9L17,5V8H10V10H17V13M7,11L3,15L7,19V16H14V14H7V11Z" /></svg>'
 
-        readButton.textContent = book.read === "Yes" ? "Read" : "Not Read";
+        bookItem.innerHTML = `<p>Title: ${book.title}</p><p>Author: ${book.author}</p><p>Pages:${book.pages}</p>`
 
         removeButton.addEventListener('click', function(){
             const bookIndex = myLibrary.indexOf(book);
@@ -54,9 +59,12 @@ function displayLibrary(){
         })
     
         removeButton.appendChild(removeIcon);
+        readButton.appendChild(readIcon);
+
         bookItem.appendChild(removeButton);
         bookItem.appendChild(readButton);
         bookItem.appendChild(bookButtons);
+
         bookButtons.appendChild(removeButton);
         bookButtons.appendChild(readButton);
 
